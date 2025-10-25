811c540e7ebb3164dc9529573287d9f2e541d062
ramaeon <Rama.eon@gmail.com>
2025-10-25 17:14:47 +0530

    Add curated Telugu rhymes content
    - Replace dummy videos with 10 popular Telugu rhymes
    - Add 10 Telugu rhymes shorts
    - Content from trusted channels: infobells, CVS 3D Rhymes, KidsOne
    - Safe, educational content perfect for children
    - Total views: 200+ crore across all videos

--- app/src/main/java/com/kidscurated/player/data/VideoRepository.kt
+++ app/src/main/java/com/kidscurated/player/data/VideoRepository.kt
@@ -12,73 +12,212 @@ data class Video(
 )
 
 object VideoRepository {
-    // Add your curated YouTube video links here
-    // Extract video ID from URLs like: https://www.youtube.com/watch?v=VIDEO_ID
-    // or from shorts: https://www.youtube.com/shorts/VIDEO_ID
+    // Curated Telugu Rhymes for Children
+    // These videos are safe, educational, and perfect for kids!
     
     val curatedVideos = listOf(
         Video(
-            id = "dQw4w9WgXcQ",
-            title = "Sample Educational Video 1",
-            channelName = "Kids Learning Channel",
-            thumbnailUrl = "https://img.youtube.com/vi/dQw4w9WgXcQ/maxresdefault.jpg",
-            views = "1.2M views",
-            uploadTime = "2 days ago",
-            duration = "5:30",
-            youtubeUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+            id = "VMXHQRLRywY",
+            title = "Top 25 Telugu Rhymes for Children - Infobells",
+            channelName = "infobells - Telugu",
+            thumbnailUrl = "https://img.youtube.com/vi/VMXHQRLRywY/maxresdefault.jpg",
+            views = "19 crore views",
+            uploadTime = "9 years ago",
+            duration = "25:12",
+            youtubeUrl = "https://www.youtube.com/watch?v=VMXHQRLRywY"
         ),
         Video(
-            id = "jNQXAC9IVRw",
-            title = "Sample Educational Video 2",
-            channelName = "Educational Kids",
-            thumbnailUrl = "https://img.youtube.com/vi/jNQXAC9IVRw/maxresdefault.jpg",
-            views = "850K views",
-            uploadTime = "1 week ago",
-            duration = "8:15",
-            youtubeUrl = "https://www.youtube.com/watch?v=jNQXAC9IVRw"
+            id = "giVecL2ANv8",
+            title = "Dam Dam Dam - 3D Animation Telugu Rhymes for Children",
+            channelName = "CVS 3D Rhymes & Kids Songs",
+            thumbnailUrl = "https://img.youtube.com/vi/giVecL2ANv8/maxresdefault.jpg",
+            views = "33 crore views",
+            uploadTime = "12 years ago",
+            duration = "3:08",
+            youtubeUrl = "https://www.youtube.com/watch?v=giVecL2ANv8"
         ),
         Video(
-            id = "9bZkp7q19f0",
-            title = "Sample Learning Video 3",
-            channelName = "Fun Learning",
-            thumbnailUrl = "https://img.youtube.com/vi/9bZkp7q19f0/maxresdefault.jpg",
-            views = "2.1M views",
-            uploadTime = "3 days ago",
-            duration = "6:45",
-            youtubeUrl = "https://www.youtube.com/watch?v=9bZkp7q19f0"
+            id = "kE8K0WomVSQ",
+            title = "Kothi Bava | Telugu Rhymes For Children | Mango Telugu Rhymes",
+            channelName = "Mango Telugu Rhymes",
+            thumbnailUrl = "https://img.youtube.com/vi/kE8K0WomVSQ/maxresdefault.jpg",
+            views = "70 lakh views",
+            uploadTime = "4 months ago",
+            duration = "2:28",
+            youtubeUrl = "https://www.youtube.com/watch?v=kE8K0WomVSQ"
+        ),
+        Video(
+            id = "OSDPdzTi_yQ",
+            title = "మా తాత అందం - Telugu Nursery Rhymes for Kids",
+            channelName = "Kids Zone",
+            thumbnailUrl = "https://img.youtube.com/vi/OSDPdzTi_yQ/maxresdefault.jpg",
+            views = "71 lakh views",
+            uploadTime = "5 months ago",
+            duration = "1:18",
+            youtubeUrl = "https://www.youtube.com/watch?v=OSDPdzTi_yQ"
+        ),
+        Video(
+            id = "phYVw1ZrSr8",
+            title = "తారంగం తారంగం | Telugu Nursery Rhymes For Kids | KidsOne Telugu",
+            channelName = "KidsOne Telugu",
+            thumbnailUrl = "https://img.youtube.com/vi/phYVw1ZrSr8/maxresdefault.jpg",
+            views = "27 lakh views",
+            uploadTime = "11 months ago",
+            duration = "2:17",
+            youtubeUrl = "https://www.youtube.com/watch?v=phYVw1ZrSr8"
+        ),
+        Video(
+            id = "VsmS35KaUyk",
+            title = "Telugu Rhymes for Children Vol. 1 - 3D Chitti Chilakamma and 23 Rhymes",
+            channelName = "CVS 3D Rhymes & Kids Songs",
+            thumbnailUrl = "https://img.youtube.com/vi/VsmS35KaUyk/maxresdefault.jpg",
+            views = "36 crore views",
+            uploadTime = "5 years ago",
+            duration = "38:27",
+            youtubeUrl = "https://www.youtube.com/watch?v=VsmS35KaUyk"
+        ),
+        Video(
+            id = "4TpW-Qfjd-0",
+            title = "Chitti Chilakamma Amma Kottinda | Telugu Rhymes for Children | Infobells",
+            channelName = "infobells - Telugu",
+            thumbnailUrl = "https://img.youtube.com/vi/4TpW-Qfjd-0/maxresdefault.jpg",
+            views = "32 crore views",
+            uploadTime = "2 years ago",
+            duration = "5:57",
+            youtubeUrl = "https://www.youtube.com/watch?v=4TpW-Qfjd-0"
+        ),
+        Video(
+            id = "RqLstPFSJ0o",
+            title = "జామ చెట్టు | Telugu Rhymes for Children by infobells",
+            channelName = "infobells - Telugu",
+            thumbnailUrl = "https://img.youtube.com/vi/RqLstPFSJ0o/maxresdefault.jpg",
+            views = "7.1 crore views",
+            uploadTime = "9 years ago",
+            duration = "1:00",
+            youtubeUrl = "https://www.youtube.com/watch?v=RqLstPFSJ0o"
+        ),
+        Video(
+            id = "0F6WRYemPRE",
+            title = "Gundrani Gundrati Laddu | Telugu Rhymes for Children | Infobells",
+            channelName = "infobells - Telugu",
+            thumbnailUrl = "https://img.youtube.com/vi/0F6WRYemPRE/maxresdefault.jpg",
+            views = "84 crore views",
+            uploadTime = "2 years ago",
+            duration = "3:14",
+            youtubeUrl = "https://www.youtube.com/watch?v=0F6WRYemPRE"
+        ),
+        Video(
+            id = "2kUaylNjr4M",
+            title = "Telugu Rhymes for Children | 27 Telugu Nursery Rhymes Collection | Telugu Baby Songs",
+            channelName = "CVS 3D Rhymes & Kids Songs",
+            thumbnailUrl = "https://img.youtube.com/vi/2kUaylNjr4M/maxresdefault.jpg",
+            views = "30 crore views",
+            uploadTime = "9 years ago",
+            duration = "31:43",
+            youtubeUrl = "https://www.youtube.com/watch?v=2kUaylNjr4M"
         )
     )
     
     val curatedShorts = listOf(
         Video(
-            id = "dQw4w9WgXcQ",
-            title = "Quick Learning Moment 1",
-            channelName = "Kids Shorts",
-            thumbnailUrl = "https://img.youtube.com/vi/dQw4w9WgXcQ/maxresdefault.jpg",
-            views = "500K views",
-            uploadTime = "1 day ago",
-            duration = "0:45",
-            youtubeUrl = "https://www.youtube.com/shorts/dQw4w9WgXcQ"
+            id = "yvoLY8U0IU4",
+            title = "Kaki Kaki Kadavala Kaki | Telugu Rhymes & Kids Songs | Infobells",
+            channelName = "infobells - Telugu",
+            thumbnailUrl = "https://img.youtube.com/vi/yvoLY8U0IU4/maxresdefault.jpg",
+            views = "1.3 crore views",
+            uploadTime = "Recent",
+            duration = "0:60",
+            youtubeUrl = "https://www.youtube.com/shorts/yvoLY8U0IU4"
         ),
         Video(
-            id = "jNQXAC9IVRw",
-            title = "Fun Fact Short 2",
-            channelName = "Educational Shorts",
-            thumbnailUrl = "https://img.youtube.com/vi/jNQXAC9IVRw/maxresdefault.jpg",
-            views = "750K views",
-            uploadTime = "2 days ago",
-            duration = "0:30",
-            youtubeUrl = "https://www.youtube.com/shorts/jNQXAC9IVRw"
+            id = "nlOeOkkFDic",
+            title = "Telugu Kids Rhymes | Enugamma Enugu | #telugurhymes - Infobells",
+            channelName = "infobells - Telugu",
+            thumbnailUrl = "https://img.youtube.com/vi/nlOeOkkFDic/maxresdefault.jpg",
+            views = "49 lakh views",
+            uploadTime = "Recent",
+            duration = "0:60",
+            youtubeUrl = "https://www.youtube.com/shorts/nlOeOkkFDic"
         ),
         Video(
-            id = "9bZkp7q19f0",
-            title = "Amazing Discovery",
-            channelName = "Science for Kids",
-            thumbnailUrl = "https://img.youtube.com/vi/9bZkp7q19f0/maxresdefault.jpg",
-            views = "1.2M views",
-            uploadTime = "5 hours ago",
-            duration = "0:55",
-            youtubeUrl = "https://www.youtube.com/shorts/9bZkp7q19f0"
+            id = "twalaOG9CGk",
+            title = "Chandamama Raave | Telugu Rhymes for Kids & Babies | Infobells",
+            channelName = "infobells - Telugu",
+            thumbnailUrl = "https://img.youtube.com/vi/twalaOG9CGk/maxresdefault.jpg",
+            views = "1 crore views",
+            uploadTime = "Recent",
+            duration = "0:60",
+            youtubeUrl = "https://www.youtube.com/shorts/twalaOG9CGk"
+        ),
+        Video(
+            id = "nB5XtiJIRN4",
+            title = "Telugu Rhymes - Nedu Mangaḷavaram DOCTOR SONG | Butta Bomma Telugu Rhymes For Children",
+            channelName = "Butta Bomma Telugu Rhymes For Children",
+            thumbnailUrl = "https://img.youtube.com/vi/nB5XtiJIRN4/maxresdefault.jpg",
+            views = "1.2 crore views",
+            uploadTime = "Recent",
+            duration = "0:60",
+            youtubeUrl = "https://www.youtube.com/shorts/nB5XtiJIRN4"
+        ),
+        Video(
+            id = "ZDJ-dMkf7Ng",
+            title = "Tappetloy Talaloyi - Little Krishna | Telugu Rhymes & Baby Songs | Infobells",
+            channelName = "infobells - Telugu",
+            thumbnailUrl = "https://img.youtube.com/vi/ZDJ-dMkf7Ng/maxresdefault.jpg",
+            views = "2.1 crore views",
+            uploadTime = "Recent",
+            duration = "0:60",
+            youtubeUrl = "https://www.youtube.com/shorts/ZDJ-dMkf7Ng"
+        ),
+        Video(
+            id = "7dnIde8lgrk",
+            title = "Vache Vache Railu Bandi - Fun Telugu Animated Train Song for Children",
+            channelName = "KidsOne",
+            thumbnailUrl = "https://img.youtube.com/vi/7dnIde8lgrk/maxresdefault.jpg",
+            views = "90K views",
+            uploadTime = "Recent",
+            duration = "0:60",
+            youtubeUrl = "https://www.youtube.com/shorts/7dnIde8lgrk"
+        ),
+        Video(
+            id = "wTXwh9rXJKU",
+            title = "చిట్టి చిలకమ్మ | Chitti Chilakamma | Telugu Rhymes For Children | Kidsone Telugu",
+            channelName = "Kidsone Telugu",
+            thumbnailUrl = "https://img.youtube.com/vi/wTXwh9rXJKU/maxresdefault.jpg",
+            views = "31 lakh views",
+            uploadTime = "Recent",
+            duration = "0:60",
+            youtubeUrl = "https://www.youtube.com/shorts/wTXwh9rXJKU"
+        ),
+        Video(
+            id = "z_O5EhnLFZU",
+            title = "Holi Vaccindi - Holi Song | Telugu Rhymes | infobells",
+            channelName = "infobells - Telugu",
+            thumbnailUrl = "https://img.youtube.com/vi/z_O5EhnLFZU/maxresdefault.jpg",
+            views = "13 crore views",
+            uploadTime = "Recent",
+            duration = "0:60",
+            youtubeUrl = "https://www.youtube.com/shorts/z_O5EhnLFZU"
+        ),
+        Video(
+            id = "qTHpKwGTtMw",
+            title = "Bath Song Part 1 Telugu Shorts | Baby Ronnie | Minnu and Mintu Telugu Nursery Rhyme",
+            channelName = "Videogyan Telugu - Nursery Rhymes & Kids Songs",
+            thumbnailUrl = "https://img.youtube.com/vi/qTHpKwGTtMw/maxresdefault.jpg",
+            views = "79 crore views",
+            uploadTime = "Recent",
+            duration = "0:60",
+            youtubeUrl = "https://www.youtube.com/shorts/qTHpKwGTtMw"
+        ),
+        Video(
+            id = "v3UWXUK1dRA",
+            title = "Chitti Chilakamma - Parrot Song | Butta Bomma Telugu Babies Rhymes",
+            channelName = "Butta Bomma Telugu Babies Rhymes",
+            thumbnailUrl = "https://img.youtube.com/vi/v3UWXUK1dRA/maxresdefault.jpg",
+            views = "54 lakh views",
+            uploadTime = "Recent",
+            duration = "0:60",
+            youtubeUrl = "https://www.youtube.com/shorts/v3UWXUK1dRA"
         )
     )
     
